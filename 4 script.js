678c08763da11e4bd8578ee47992a1cf35e3df7f
arquelau casa santos <arquelaucompra@gmail.com>
2025-06-12 01:34:13 -0300

    tcc
    tcc

--- 4 script.js
+++ 4 script.js
@@ -4,18 +4,20 @@ const options = {
 
     strings: [
         "BEM VINDO A BE YOU ESTÉTICA",
-        "estética a vançada e integrada",
-        " comunicação clara e eficiente é essencial para explicar procedimentos e resultados",
-        " personalização do atendimento é crucial para fidelizar clientes."
+        "O atendimento em clínicas de estética deve focar na empatia, escuta ativa e um ambiente acolhedor. A  comunicação clara e eficiente é essencial para explicar procedimentos e resultados, e a  personalização do atendimento é crucial para fidelizar clientes.", 
+           
+        " Um sistema de agendamento eficiente, um ambiente físico agradável e treinamento da equipe são também importantes par um bom atendimento."
+        
+    
     ],
 
 
-typeSpeed: 60, // velocidade da digitação (em milisegundos por caractere)
-backSpeed: 40, // velocidade em apagar os texto
-loop: true,    // faz com que as frases se repitam indefinidamente
-backDelay: 1500, // tempo de espera (em milisegundo) antes de começar a apagar uma frase
-smartBacKspace: true // otimiza o apagamento : apaga apenas as caratecteres diferente da proxima frase
- 
+    typeSpeed: 60, // velocidade da digitação (em milisegundos por caractere)
+    backSpeed: 60, // velocidade em apagar os texto
+    loop: true,    // faz com que as frases se repitam indefinidamente
+    backDelay: 2500, // tempo de espera (em milisegundo) antes de começar a apagar uma frase
+    smartBacKspace: true // otimiza o apagamento : apaga apenas as caratecteres diferente da proxima frase
+
 }
 
 
